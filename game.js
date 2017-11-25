3bd203347627359bb32b12a7904228283576cf88
EdwardGoomba <ask@edwardvdesign.com>
2017-11-25 13:49:17 -0500

    Added new platform type. Added additional platforms and coins.

--- game.js
+++ game.js
@@ -18,11 +18,12 @@ function addItems() {
   createItem(375, 400, 'coin');
   createItem(575, 500, 'coin');
   createItem(225, 500, 'coin');
-  createItem(550, 450, 'coin');
-  createItem(580, 350, 'coin');
-  createItem(300, 250, 'coin');
-  createItem(70, 200, 'coin');
-  createItem(600, 150, 'coin');
+  createItem(100, 250, 'coin');
+  createItem(575, 150, 'coin');
+  createItem(525, 300, 'coin');
+  createItem(650, 250, 'coin');
+  createItem(225, 200, 'coin');
+  createItem(375, 100, 'coin');
 }
 
 // add platforms to the game
@@ -30,11 +31,14 @@ function addPlatforms() {
   platforms = game.add.physicsGroup();
   platforms.create(450, 550, 'platform');
   platforms.create(100, 550, 'platform');
+  platforms.create(300, 450, 'platform2');
+  platforms.create(250, 150, 'platform');
+  platforms.create(50, 300, 'platform');
+  platforms.create(150, 250, 'platform2');
+  platforms.create(650, 300, 'platform');
+  platforms.create(550, 200, 'platform2');
   platforms.create(300, 450, 'platform');
-  platforms.create(550, 400, 'platform');
-  platforms.create(300, 300, 'platform');
-  platforms.create(70, 250, 'platform');
-  platforms.create(600, 200, 'platform');
+  platforms.create(400, 350, 'platform2');
   platforms.setAll('body.immovable', true);
 }
 
@@ -78,6 +82,7 @@ window.onload = function () {
 
     //Load images
     game.load.image('platform', 'platform_1.png');
+    game.load.image('platform2', 'platform_2.png');
 
     //Load spritesheets
     game.load.spritesheet('player', 'chalkers.png', 48, 62);
